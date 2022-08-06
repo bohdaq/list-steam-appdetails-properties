26dffd2797df78f12ba9e7b0ed1767a7b6dfc750
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-08-06 22:31:05 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use std::fs::read_to_string;
 use steam_webapi_rust_sdk::store_steampowered_com::appdetails::get_resource_filepath;
 
@@ -14,7 +15,43 @@ fn main() {
     if is_readable {
         let cached_api_response = boxed_read.unwrap();
         println!("{}", cached_api_response);
+        parse_json(cached_api_response, dota_app_id);
     } else {
         println!("unable to read cached resource");
+    }
+
+
+}
+
+fn parse_json(cached_api_response: String, app_id: i64) {
+    let boxed_initial_parse = serde_json::from_str(&cached_api_response);
+    if boxed_initial_parse.is_err() {
+        return println!("{}", boxed_initial_parse.err().unwrap().to_string());
+    }
+    let mut json: Value = boxed_initial_parse.unwrap();
+
+    let mut app_details_wrapped = json[app_id.to_string()].take();
+
+    let mut is_success = app_details_wrapped["success".to_string()].take();
+    if is_success.take().as_bool().unwrap() == false {
+        println!("{}", "steampowered api returned failed response".to_string());
+    }
+
+    let mut app_details : Value = app_details_wrapped["data"].take();
+    let mut indendation = 0;
+    parse_json_object(&app_details, indendation)
+}
+
+fn parse_json_object(json: &Value, mut indentation: i64) {
+    for (key, value) in json.as_object().unwrap() {
+        let literal = " ".repeat(indentation as usize);
+        println!("{}{}", literal, key);
+
+        if value.is_object() {
+            indentation = indentation + 1;
+            parse_json_object(value, indentation);
+            indentation = indentation - 1;
+        }
+
     }
 }
